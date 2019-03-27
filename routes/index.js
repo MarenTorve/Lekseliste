e7acaaf101807d049536520ea49a5081ff53f426
Maren Torve <marent16@student.uia.no>
2019-03-27 14:22:58 +0100

    Heroku test 2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+const path = require('path');
+const pathHTML = path.join(__dirname, "../API/HTML");
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile( pathHTML + "/index.html");
 });
 
 module.exports = router;
